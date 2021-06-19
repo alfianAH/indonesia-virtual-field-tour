fda57ed97a61dcb530a01323d71efaf5a9c1f528
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-06-19 16:23:27 +0800

    Add audio manager

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,107 @@
+using System;
+using UnityEngine;
+
+namespace Audio
+{
+    public class AudioManager : MonoBehaviour
+    {
+        #region Singleton
+
+        private static AudioManager instance;
+        private const string LOG = nameof(AudioManager);
+        
+        /// <summary>
+        /// Singleton method
+        /// </summary>
+        public static AudioManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    // Find instance
+                    instance = FindObjectOfType<AudioManager>();
+                    
+                    // If instance is not found, ...
+                    if (instance == null)
+                    {
+                        // Give log error
+                        Debug.LogError($"{LOG} not found");
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion
+
+        #region Don't Destroy On Load
+        
+        /// <summary>
+        /// Use only 1 Audio manager
+        /// </summary>
+        private void SetInstance()
+        {
+            if (instance != null && instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+        
+
+        #endregion
+
+        public Sound[] sounds;
+
+        private void Awake()
+        {
+            SetInstance();
+
+            foreach (Sound sound in sounds)
+            {
+                // Make new Audio Source for each sound
+                sound.source = gameObject.AddComponent<AudioSource>();
+                
+                // Set Audio Source's properties
+                sound.source.playOnAwake = false;
+                sound.source.clip = sound.clip;
+                sound.source.loop = sound.loop;
+                sound.source.pitch = sound.pitch;
+                sound.source.volume = sound.volume;
+            }
+        }
+        
+        /// <summary>
+        /// Play audio
+        /// </summary>
+        /// <param name="listSound">Type of sound that want to be played</param>
+        public void Play(ListSound listSound)
+        {
+            GetAudioSource(listSound).Play();
+        }
+        
+        /// <summary>
+        /// Get audio source for the right listSound
+        /// </summary>
+        /// <param name="listSound">Type of sound that want to be played</param>
+        /// <returns>Returns listSound's audio source</returns>
+        private AudioSource GetAudioSource(ListSound listSound)
+        {
+            Sound s = Array.Find(sounds, sound => sound.listSound == listSound);
+
+            if (s == null)
+            {
+                Debug.LogError($"Sound: {listSound} not found!");
+                return null;
+            }
+
+            return s.source;
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Audio
+{
+    [Serializable]
+    public class Sound
+    {
+        public ListSound listSound;
+
+        public AudioClip clip;
+
+        [Range(0, 1)] public float volume;
+        [Range(-3, 3)] public float pitch = 1;
+
+        public bool loop;
+        public AudioSource source;
+    }
+
+    public enum ListSound
+    {
+        Marker1,
+        Marker2,
+        Marker3
+    }
+}
