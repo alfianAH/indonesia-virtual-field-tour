4d86d622a7c18ad43095a91b9e38b22e3377d1d0
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-06-19 16:38:25 +0800

    Add camera focus script

---
+++ Assets/Scripts/Camera/CameraFocus.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+using Vuforia;
+
+namespace Camera
+{
+    public class CameraFocus : MonoBehaviour
+    {
+        private void Start()
+        {
+            var vuforia = VuforiaARController.Instance;
+            
+            vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+            vuforia.RegisterOnPauseCallback(OnPaused);
+        }
+        
+        /// <summary>
+        /// On Vuforia started, set camera focus mode
+        /// </summary>
+        private void OnVuforiaStarted()
+        {
+            CameraDevice.Instance.SetFocusMode(
+                CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        }
+        
+        /// <summary>
+        /// On paused
+        /// </summary>
+        /// <param name="paused"></param>
+        private void OnPaused(bool paused)
+        {
+            // When resumed, ...
+            if (!paused)
+            {
+                // Set again auto focus mode when app is resumed
+                CameraDevice.Instance.SetFocusMode(
+                    CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+            }
+        }
+    }
+}
