e329ccea286488d75be9c9bbd5f3ab12772b8526
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-08-31 00:01:21 +0800

    Access generic class name in singleton base class

--- Assets/Scripts/SingletonBaseClass.cs
+++ Assets/Scripts/SingletonBaseClass.cs
@@ -3,23 +3,20 @@
 public class SingletonBaseClass<T>: MonoBehaviour where T: MonoBehaviour 
 {
     private static T instance;
-    private const string LOG = nameof(T);
 
     public static T Instance
     {
         get
         {
+            string log = typeof(T).Name;
+
+            if (instance != null) return instance;
+            
+            instance = FindObjectOfType<T>();
             if (instance == null)
             {
-                Debug.Log($"Find {LOG}");
-                instance = FindObjectOfType<T>();
-                if (instance == null)
-                {
-                    Debug.LogError($"{LOG} tidak ditemukan");
-                }
+                Debug.LogError($"{log} tidak ditemukan");
             }
-            
-            Debug.Log($"Get {LOG}");
 
             return instance;
         }
